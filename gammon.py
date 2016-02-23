e41b14765ab32b7aa12c0c7112a3b62776171927
elcocodrilo <robertdumbleton@gmail.com>
2016-02-23 15:35:12 -0800

    minor additions to gammon board

--- gammon.py
+++ gammon.py
@@ -1,18 +1,26 @@
 ## playable backgammon
 
+## create the structure of the board a list of 24 elements, each element contains a list of 2 elements
+## the first is either ' ' (empty space), 'B' (black occupied) or 'W' (white occupied)
+## the second is either ' ' (if empty, or a number representing how many pieces are on the space)
+
 board = [[' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '],
  [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '],
   [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '],
    [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' ']]
 
+## updates board to show starting position
 board = [['B', 2], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], ['W', 5],
  [' ', ' '], ['W', 3], [' ', ' '], [' ', ' '], [' ', ' '], ['B', 5],
   ['W', 5], [' ', ' '], [' ', ' '], [' ', ' '], ['B', 3], [' ', ' '],
    ['B', 5], [' ', ' '], [' ', ' '], [' ', ' '], [' ', ' '], ['W', 2]]
 
+jail = [['B', 0], ['W', 0]]
+offboard = [['B', 0], ['W', 0]]
+
 def draw_board(board):
 
-    print ' ---------------------------------------------------------------------------------------'
+    print '\n ---------------------------------------------------------------------------------------'
     print ' |\  ' + board[0][0] + '  /\  ' + board[1][0] + '  /\  ' + board[2][0] + '  /\  ' + \
     board[3][0] + '  /\  ' + board[4][0] + '  /\  ' + board[5][0] + '  /|\  ' + \
     board[6][0] + '  /\  ' + board[7][0] + '  /\  ' + board[8][0] + '  /\  ' + \
@@ -34,6 +42,8 @@ def draw_board(board):
     board[20][0] + '  \/  ' + board[19][0] + '  \/  ' + board[18][0] + '  \|/  ' + \
     board[17][0] + '  \/  ' + board[16][0] + '  \/  ' + board[15][0] + '  \/  ' + \
     board[14][0] + '  \/  ' + board[13][0] + '  \/  ' + board[12][0] + '  \| '
-    print ' ---------------------------------------------------------------------------------------'
+    print ' ---------------------------------------------------------------------------------------\n'
+    print 'Black has: ' + str(jail[0][1]) + ' pieces in jail, and ' + str(offboard[0][1]) + ' pieces off the board\n'
+    print 'White has: ' + str(jail[1][1]) + ' pieces in jail, and ' + str(offboard[1][1]) + ' pieces off the board\n'
 
 print draw_board(board)
